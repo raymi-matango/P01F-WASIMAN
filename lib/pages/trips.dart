730df1ebb443c9a6c096fddfb925905a6aaf8832
Kutik-Gomez <kutikgomez@gmail.com>
2024-06-05 22:38:04 -0500

    prueba de token imprimiendo

--- lib/pages/trips.dart
+++ lib/pages/trips.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:iniciofront/auth/login.dart';
 
 class ViajePagina extends StatefulWidget {
   const ViajePagina({Key? key}) : super(key: key);
@@ -11,79 +9,32 @@ class ViajePagina extends StatefulWidget {
 }
 
 class _ViajePaginaState extends State<ViajePagina> {
-  List<dynamic> viajes = [];
+  String? token;
 
   @override
   void initState() {
     super.initState();
-    _fetchViajes();
+    _loadToken();
   }
 
-  Future<void> _fetchViajes() async {
-    final token = await TokenManager.getToken();
-
-    if (token == null) {
-      // Manejar el caso en que el token no esté disponible
-      print('Token no disponible');
-      return;
-    }
-
-    final response = await http.get(
-      Uri.parse('http://localhost:7777/api/viajes/listar'),
-      headers: {'Authorization': 'Bearer $token'},
-    );
-
-    if (response.statusCode == 200) {
-      setState(() {
-        viajes = json.decode(response.body);
-      });
-    } else {
-      // Manejar el error de manera apropiada
-      print('Error al cargar los viajes: ${response.statusCode}');
-    }
+  Future<void> _loadToken() async {
+    final retrievedToken = await TokenManager.getToken();
+    setState(() {
+      token = retrievedToken;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de Viajes'),
+        title: Text('Viaje Página'),
       ),
-      body: viajes.isEmpty
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: viajes.length,
-              itemBuilder: (context, index) {
-                final viaje = viajes[index];
-                return ListTile(
-                  title: Text(viaje['destino'] ?? ''),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Fecha de Salida: ${viaje['fechaSalida'] ?? ''}'),
-                      Text('Hora de Salida: ${viaje['horaSalida'] ?? ''}'),
-                      Text(
-                          'Puntos de Recogida: ${viaje['puntosRecogida'] ?? ''}'),
-                      Text(
-                          'Capacidad de Asientos: ${viaje['capacidadAsientos'] ?? ''}'),
-                      Text('Detalles: ${viaje['detalles'] ?? ''}'),
-                      Text('Disponible: ${viaje['disponible'] ?? ''}'),
-                    ],
-                  ),
-                );
-              },
-            ),
+      body: Center(
+        child: token != null
+            ? Text('Token: $token')
+            : CircularProgressIndicator(), // Muestra un indicador de carga mientras se obtiene el token
+      ),
     );
-  }
-}
-
-class TokenManager {
-  static const _key = 'jwt_token';
-
-  static Future<String?> getToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_key);
   }
 }
