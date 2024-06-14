da2cc2bb90346602a087cd30851ee334ec1b9147
Kutik-Gomez <kutikgomez@gmail.com>
2024-06-14 01:43:32 -0500

    cambio de notificacion

--- lib/pages/screens/notificacion.dart
+++ lib/pages/screens/notificacion.dart
@@ -12,53 +12,59 @@ class Notificacion extends StatefulWidget {
 class _NotificacionState extends State<Notificacion> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notificación'),
       ),
-      margin: EdgeInsets.all(16.0),
-      color: Color(0xFFF2CA50),
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '¡Bienvenido a la aplicación!',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF0E402E),
+      body: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        margin: EdgeInsets.all(16.0),
+        color: Color(0xffF1F2F0),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '¡Bienvenido a la aplicación!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF0E402E),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Ingresaste el ${widget.fechaIngreso} para iniciar con la reserva.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Color(0xFF688C6A),
+              SizedBox(height: 16),
+              Text(
+                'Ingresaste el ${widget.fechaIngreso} para iniciar con la reserva.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xffF29F05),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    // Agregar aquí la funcionalidad del botón si es necesario
-                  },
-                  child: Text(
-                    'Cerrar',
-                    style: TextStyle(
-                      color: Color(0xFFBF8756),
-                      fontSize: 16,
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Cerrar',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 242, 68, 5),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
